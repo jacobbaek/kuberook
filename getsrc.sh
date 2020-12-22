5e552378a44438dd594fcc4b6dc855b8c8a2b851
root <root@jacobbaek-deploy.novalocal>
2020-12-22 01:06:15 +0000

    Update yaml files that can deploy osds

--- getsrc.sh
+++ getsrc.sh
@@ -1,3 +1,6 @@
 
 
+pip3 install -r requirements.txt
 git clone https://github.com/kubernetes-sigs/kubespray.git
+cp -rf kubespray/inventory/sample kubespray/inventory/jacob
+
--- yamls/clean-rook.sh
+++ yamls/clean-rook.sh
@@ -7,3 +7,8 @@ do
     ssh $node rm -rf /var/lib/kubelet/plugins_registry/rook-ceph*
     echo "delete rook-ceph files in "$node
 done
+
+for pod in `kubectl get secret -n rook-ceph -o name`;
+do
+    kubectl delete $pod -n rook-ceph
+done
